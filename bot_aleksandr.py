def334fc48ca2f92f0d063c7e2f5014fccc0604f
alek-ulitin <alek-ulitin@yandex.ru>
2022-03-15 23:08:50 +0300

    init

--- bot_aleksandr.py
+++ bot_aleksandr.py
@@ -8,26 +8,33 @@ bot = telebot.TeleBot("5123285996:AAHbe3pz1bgjBpszNBDIsI003htyjXp5mxs")
 @bot.message_handler(content_types=["text"])
 def get_text_message(message):
 
-    if message.text == "/start" or message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, я бот который показывает актуальный курс валюты")
+    bot.send_message(message.from_user.id, "Привет, я бот который показывает актуальный курс валюты" +"\n" + "напиши мне: Привет")
+    if message.text == "привет" or message.text == "Привет":
+        keyboard = types.InlineKeyboardMarkup()
+        key_lira = types.InlineKeyboardButton(text="Турецкая Лира", callback_data="lira")
+        keyboard.add(key_lira)
+        key_dollar = types.InlineKeyboardButton(text="Американский доллар", callback_data="dollar")
+        keyboard.add(key_dollar)
+        key_euro = types.InlineKeyboardButton(text="Евро", callback_data="euro")
+        keyboard.add(key_euro)
+
+        bot.send_message(message.from_user.id, text="Выбери валюту", reply_markup=keyboard)
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю... Напиши: Привет")
-
-    keyboard = types.InlineKeyboardMarkup()
-    key_lira = types.InlineKeyboardButton(text="Турецкая Лира", callback_data="lira")
-    keyboard.add(key_lira)
-    key_dollar = types.InlineKeyboardButton(text="Американский доллар", callback_data="dollar")
-    keyboard.add(key_dollar)
-    key_euro = types.InlineKeyboardButton(text="Евро", callback_data="euro")
-    keyboard.add(key_euro)
-
-    bot.send_message(message.from_user.id, text="Выбери валюту", reply_markup=keyboard)
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback_worker(call):
         if call.data == "lira":
             msg = parser_currency.check_currency_lira()
             bot.send_message(call.message.chat.id, msg)
+        elif call.data == "dollar":
+            msg = parser_currency.check_currency_dollar()
+            bot.send_message(call.message.chat.id, msg)
+        elif call.data == "euro":
+            msg = parser_currency.check_currency_euro()
+            bot.send_message(call.message.chat.id, msg)
+        else:
+            print("Такой валюты нет в боте")
 
 
 bot.polling(none_stop=True, interval=0)
--- parser_currency.py
+++ parser_currency.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+EURO_RUB = "https://www.google.com/search?q=rehc+tdhj&oq"
+DOLLAR_RUB = "https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D0%B0"
 LIRA_RUB = "https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%BB%D0%B8%D1%80%D1%8B"
-Headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"" Chrome/"
-                      "99.0.4844.51 Safari/537.36"}
+Headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"" Chrome/"
+                             "99.0.4844.51 Safari/537.36"}
 
 
 def check_currency_lira():
-    return float(get_currency_price_lira().replace(",", "."))
+    return str(float(get_currency_price_lira().replace(",", "."))) + " рублей"
 
 
 def get_currency_price_lira():
@@ -21,5 +22,29 @@ def get_currency_price_lira():
     return convert[0].text
 
 
-if __name__ == '__main__':
-    check_currency_lira()
+def check_currency_euro():
+    return str(float(get_currency_price_euro().replace(",", "."))) + " рублей"
+
+
+def get_currency_price_euro():
+    full_page = requests.get(EURO_RUB, headers=Headers)
+
+    soup = BeautifulSoup(full_page.content, "html.parser")
+
+    convert = soup.findAll("span", {"class": "DFlfde", "data-precision": "2"})
+
+    return convert[0].text
+
+
+def check_currency_dollar():
+    return str(float(get_currency_price_dollar().replace(",", "."))) + " рублей"
+
+
+def get_currency_price_dollar():
+    full_page = requests.get(DOLLAR_RUB, headers=Headers)
+
+    soup = BeautifulSoup(full_page.content, "html.parser")
+
+    convert = soup.findAll("span", {"class": "DFlfde", "data-precision": "2"})
+
+    return convert[0].text
