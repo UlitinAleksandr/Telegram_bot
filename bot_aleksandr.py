47e7b3258a5152b9336d5cb54458d4e60249fa68
Aleksandr_Ulitin <alek-ulitin@mail.ru>
2022-11-25 16:18:31 +0300

    init

--- bot_aleksandr.py
+++ bot_aleksandr.py
@@ -4,33 +4,28 @@ from telebot import types
 
 bot = telebot.TeleBot("5103795659:AAFruz4cZExRCRSnvg9mQObTTfcUQEAOHuk")
 
-
 @bot.message_handler(content_types=["text"])
 def get_text_message(message):
 
     keyboard = types.InlineKeyboardMarkup()
-    key_lira = types.InlineKeyboardButton(text="Турецкая Лира", callback_data="lira")
+    key_lira = types.InlineKeyboardButton(text="Turkish lira", callback_data="lira")
     keyboard.add(key_lira)
-    key_dollar = types.InlineKeyboardButton(text="Американский доллар", callback_data="dollar")
+    key_dollar = types.InlineKeyboardButton(text="American dollar", callback_data="dollar")
     keyboard.add(key_dollar)
-    key_euro = types.InlineKeyboardButton(text="Евро", callback_data="euro")
+    key_euro = types.InlineKeyboardButton(text="Euro", callback_data="euro")
     keyboard.add(key_euro)
-
-    bot.send_message(message.from_user.id, text="Выбери валюту", reply_markup=keyboard)
+    bot.send_message(message.from_user.id, text="Choose currency", reply_markup=keyboard)
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback_worker(call):
+        pc = parser_currency
         if call.data == "lira":
-            msg = parser_currency.check_currency_lira()
-            bot.send_message(call.message.chat.id, msg)
-        elif call.data == "dollar":
-            msg = parser_currency.check_currency_dollar()
-            bot.send_message(call.message.chat.id, msg)
+            msg = pc.check(pc.LIRA_RUB)
         elif call.data == "euro":
-            msg = parser_currency.check_currency_euro()
-            bot.send_message(call.message.chat.id, msg)
-        else:
-            print("Такой валюты нет в боте")
+            msg = pc.check(pc.EURO_RUB)
+        elif call.data == "dollar":
+            msg = pc.check(pc.DOLLAR_RUB)
+        bot.send_message(call.message.chat.id, msg)
 
 
 bot.polling(none_stop=True, interval=0)
--- parser_currency.py
+++ parser_currency.py
@@ -1,50 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-EURO_RUB = "https://www.google.com/search?q=rehc+tdhj&oq"
-DOLLAR_RUB = "https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D0%B0"
-LIRA_RUB = "https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%BB%D0%B8%D1%80%D1%8B"
+EURO_RUB = "https://goo.su/euro_pa"
+DOLLAR_RUB = "https://goo.su/dollar"
+LIRA_RUB = "https://goo.su/lira"
 Headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"" Chrome/"
                              "99.0.4844.51 Safari/537.36"}
 
 
-def check_currency_lira():
-    return "1 Турецкая лира = " + str(float(get_currency_price_lira().replace(",", "."))) + " рубля(ей)"
+def check(value):
+    return str(float(get_value(value).replace(',', '.'))) + " RUB"
 
 
-def get_currency_price_lira():
-    full_page = requests.get(LIRA_RUB, headers=Headers)
-
+def get_value(value):
+    full_page = requests.get(value, headers=Headers)
     soup = BeautifulSoup(full_page.content, "html.parser")
-
     convert = soup.findAll("span", {"class": "DFlfde", "data-precision": "2"})
-
     return convert[0].text
 
-
-def check_currency_euro():
-    return "1 Евро = " + str(float(get_currency_price_euro().replace(",", "."))) + " рубля(ей)"
-
-
-def get_currency_price_euro():
-    full_page = requests.get(EURO_RUB, headers=Headers)
-
-    soup = BeautifulSoup(full_page.content, "html.parser")
-
-    convert = soup.findAll("span", {"class": "DFlfde", "data-precision": "2"})
-
-    return convert[0].text
-
-
-def check_currency_dollar():
-    return "1 Американский доллар = " + str(float(get_currency_price_euro().replace(",", "."))) + " рубля(ей)"
-
-
-def get_currency_price_dollar():
-    full_page = requests.get(DOLLAR_RUB, headers=Headers)
-
-    soup = BeautifulSoup(full_page.content, "html.parser")
-
-    convert = soup.findAll("span", {"class": "DFlfde", "data-precision": "2"})
-
-    return convert[0].text
